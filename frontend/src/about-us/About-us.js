125e223c740c3b0e19ff2d5b76e27897077795eb
unknown <wasadar2@gmail.com>
2024-09-19 17:12:34 +0300

    finished about us section, updated a little bit other anchored links

--- frontend/src/about-us/About-us.js
+++ frontend/src/about-us/About-us.js
@@ -1,10 +1,14 @@
 import SectionHeader from "../section-header/Section-header";
+import AboutUsPoint from "./point/About-us-point";
 import "./About-us.css";
 
 export default function AboutUs(props) {
-    return <>
-        <div className="AboutUsContainer">
-            <SectionHeader additionalClass="AboutUsHeaderWidth" text="ABOUT US"/>
+    return <div className="AboutUsContainer" id="about">
+        <SectionHeader additionalClass="AboutUsHeaderWidth" text="ABOUT US"/>
+        <div className="AboutUsPoints">
+            <AboutUsPoint direction={true} imageClass="AboutUsImage1" image="image1" text="We provide a unique and comprehensive online service for creating a resume and finding a job in the IT field. Our platform helps job seekers in the IT industry present their skills and experience in the best possible light, and makes it easy and efficient to search for suitable vacancies."/>
+            <AboutUsPoint direction={false} imageClass="AboutUsImage2" image="image2" text="Our platform uses advanced technologies and tools to ensure the high performance and reliability of our service. Our platform is optimized for fast page loading and easy navigation for a smooth user experience. We also guarantee the safety of your personal data and ensure the confidentiality of all information."/>
+            <AboutUsPoint direction={true} imageClass="AboutUsImage3" image="image3" text="We are proud that our online service is becoming a reliable partner for those who seek to successfully develop in the IT field. Whether you are a beginner or a seasoned professional, our platform will provide you with all the tools and resources you need to further develop and succeed in your career."/>
         </div>
-    </>
+    </div>
 }
--- frontend/src/advantages/Advantages.js
+++ frontend/src/advantages/Advantages.js
@@ -4,15 +4,12 @@ import SectionHeader from "../section-header/Section-header";
 import "./Advantages.css";
 
 export default function Advantages(props) {
-    return <>
-        <div className="AdvantagesContainer">
-                <SectionHeader additionalClass="AdvantagesHeaderWidth" text="OUR ADVANTAGES"/>
-                <a name="advantages"/>
-                <div className="Advantages">
-                    <Advantage image="image1" header="Save time" text="Create a resume in a matter of minutes thanks to an easy-to-understand interface. No more spending hours on formatting and structuring." />
-                    <Advantage image="image2" header="Resume creation" text="Create professional resumes easily, create modern and industry-specific resumes." />
-                    <Advantage image="image3" header="AI analysis" text="Get a detailed analysis of your resume using artificial intelligence, which will tell you what you can improve to increase your chances of success." />
-                </div>
-            </div>
-    </>
+    return <div className="AdvantagesContainer" id="advantages">
+        <SectionHeader additionalClass="AdvantagesHeaderWidth" text="OUR ADVANTAGES"/>
+        <div className="Advantages">
+            <Advantage image="image1" header="Save time" text="Create a resume in a matter of minutes thanks to an easy-to-understand interface. No more spending hours on formatting and structuring." />
+            <Advantage image="image2" header="Resume creation" text="Create professional resumes easily, create modern and industry-specific resumes." />
+            <Advantage image="image3" header="AI analysis" text="Get a detailed analysis of your resume using artificial intelligence, which will tell you what you can improve to increase your chances of success." />
+        </div>
+    </div>
 }
