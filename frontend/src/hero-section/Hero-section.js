4d2db1c97a1fb3ac64f5f9b54338aa9ac5a65516
Tiurne Viktoriya <tiurne@ukr.net>
2024-09-24 10:38:06 +0300

    added root constants

--- frontend/src/hero-section/Hero-section.js
+++ frontend/src/hero-section/Hero-section.js
@@ -1,6 +1,7 @@
 import image from './Hero-section.svg';
 import icon from './Button-icon.svg';
 import './Hero-section.css';
+import Button from '../UI/Button/Button';
 
 export default function HeroSection(props) {
     return <div className="HeroSection">
@@ -11,10 +12,14 @@ export default function HeroSection(props) {
             <h3 className="HeroSectionText">
                 We will help you create a resume with which you received an offer!
             </h3>
-            <a href="#join" className="HeroSectionButton">
+            <Button onClick={() => { }} className="HeroSectionButton">
+            <img src={icon} alt="HeroSectionIcon" className="HeroSectionIcon"/>
+            Try it first
+        </Button>
+            {/* <a href="#join" className="HeroSectionButton">
                 <img src={icon} alt="HeroSectionIcon" className="HeroSectionIcon"/>
                 Try it first
-            </a>
+            </a> */}
         </div>
         <img src={image} alt="HeroSectionImage" className="HeroSectionImage"/>
     </div>
