f181888b156448e8c01e3b4a5a1389472a485db9
Tiurne Viktoriya <tiurne@ukr.net>
2024-10-03 11:22:22 +0300

    chenged landingPage

--- landingPage/src/advantages/advantage/Advantage.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./Advantage.module.css";
-
-export default function Advantage(props) {
-    return <div className={styles.Advantage}>
-        <img src={require(`../${props.image}.svg`)} alt="AdvantageIcon" className={styles.AdvantageImage}/>
-        <h4 className={styles.AdvantageHeader}>{props.header}</h4>
-        <p className={styles.AdvantageText}>{props.text}</p>
-    </div>
-}
