3a2f0f3530ba40d4cc20502ecf1fd65d32e4ded1
aleksname <aleksname.v2@gmail.com>
2024-10-08 12:52:36 +0300

    change: adaptation my block

--- client/src/components/DreamBlock/DreamBlock.jsx
+++ client/src/components/DreamBlock/DreamBlock.jsx
@@ -11,7 +11,7 @@ export default function DreamBlock() {
                     <img src={DreamBlockIcon} alt="DreamBlockIcon" className={styles.dreamContentIcon} />  
                 </div> 
               <div className={styles.dreamBlockContentElement}>
-                <h2 className={styles.dreamContentTitle}>Your resume is the key to your <span className={styles.centeredLine}>dream job!</span></h2>  
+                <h2 className={styles.dreamContentTitle}>Your resume is the key to your dream job!</h2>  
                 <div className={styles.dreamContentSubTitle}>Our service helps you create a professional resume in minutes. Thanks to artificial intelligence, you will receive analysis and recommendations for improvement to stand out from other candidates.</div>  
                 <div className={styles.dreamContentButtonContainer}>
                     <Button className={styles.dreamContentButton}>Try it free</Button>
--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -11,8 +11,8 @@ const HomePage = () => {
       {/* <Header /> */}
       <HeroBlock />
       <DreamBlock />
-      <OurService />
-      {/* <Footer/> */}
+      {/* <OurService /> */}
+      <Footer/>
     </>
   );
 };
