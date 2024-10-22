03a64d1a2edac7734264730399c0e49c56205a59
aleksname <aleksname.v2@gmail.com>
2024-10-22 11:28:17 +0300

    feat: adding active resume, status: so-so

--- client/src/pages/UserPage/Main/Builder/Builder.jsx
+++ client/src/pages/UserPage/Main/Builder/Builder.jsx
@@ -1,12 +1,147 @@
-
+import Button from "../../../../components/UI/Button/Button";
 import styles from "./Builder.module.css";
 
-const Builder = () => {
+import React from 'react';
+import BuilderDropDown from "./BuilderDropDown/BuilderDropDown";
+
+import ScoreResumeCircle from "../UserHome/TopResume/ScoreResumeCircle/ScoreResumeCircle";
+
+import { ReactComponent as Remove } from '../../../../assets/user_page/builder/ActiveResume/Resume builder/Personal cabinet/pajamas_remove-all.svg'
+import { ReactComponent as Block } from '../../../../assets/user_page/builder/ActiveResume/Resume builder/Personal cabinet/majesticons_lock-line.svg'
+import { ReactComponent as Copy } from '../../../../assets/user_page/builder/ActiveResume/Resume builder/Personal cabinet/bx_duplicate.svg'
+import { ReactComponent as Edit } from '../../../../assets/user_page/builder/ActiveResume/Resume builder/Personal cabinet/lucide_edit.svg'
+// import removeIcon from ''
+
+export default function Builder({className}){
   return (
     <div className={styles.container}>
-      <div>Таблиця з резюмешками (Alexey)</div>
+      <div className={styles.bilderContantContainerNav}>
+        <Button children={'Active Resumes'} className={styles.bilderNavLink } />
+        <Button children={'Active Resumes'} className={styles.bilderNavLink } />
+      </div>
+      <div className={styles.bilderContantContainer}>
+        <div className={styles.containerTitle}>
+          <div className={styles.titleElement}>
+            <input type="checkbox" name="" id="" className={styles.checkBox} />
+            <BuilderDropDown title={ 'Resume title'} cheldrentext={'From A to Z'}  cheldrentext2={'From Z to A'} />
+          </div>
+          <nav className={styles.bilderNavTitle}>
+            <div className={styles.titleElement}>
+              <BuilderDropDown title={ 'Analyse score'} cheldrentext={'Ascending'}  cheldrentext2={'Descending'}/>
+            </div>
+            <div className={styles.titleElement}>
+              <BuilderDropDown title={ 'Creation date'} cheldrentext={'Ascending'}  cheldrentext2={'Descending'}/>
+            </div>
+            <div className={styles.titleElement}>
+              <BuilderDropDown title={ 'Edit date'} cheldrentext={'Ascending'}  cheldrentext2={'Descending'}/>
+            </div>
+          </nav>
+          <div className={styles.titleElement}>Actions</div>
+        </div>
+        <div className={styles.bilderTableContainer}>
+          <div className={styles.tableElement}>
+            <input type="checkbox" name="" id="" className={styles.checkBox} />
+            <div className={styles.bilderTablePosition}>Customer search manager</div>
+          </div>
+          <nav className={styles.bilderNavTitles}>
+            <div className={styles.tableElement}>
+              <ScoreResumeCircle
+                  size={50}
+                  strokeWidth={5}
+                  progress={89}
+                  colorProgress='#958060'
+              >
+                  <div className={styles.sectResumeDescScore}>
+                      <p className={styles.sectResumeScorePercent}>89</p>
+                      <p className={styles.sectResumeScoreText}>100</p>
+                  </div>
+              </ScoreResumeCircle>
+            </div>
+            <div className={styles.tableElement}>
+              <div className="">28 Fabruary 2024</div>
+            </div>
+            <div className={styles.tableElement}>
+              <div className="">23 March 2024</div>
+            </div>
+          </nav>
+          <div className={styles.tableElement}>
+            <a href="#" className={styles.tableAction}><Remove/></a>
+            <a href="#" className={styles.tableAction}><Block/></a>
+            <a href="#" className={styles.tableAction}><Copy/></a>
+            <a href="#" className={styles.tableAction}><Edit/></a>
+          </div>
+        </div>
+         <div className={styles.bilderTableContainer}>
+          <div className={styles.tableElement}>
+            <input type="checkbox" name="" id="" className={styles.checkBox} />
+            <div className={styles.bilderTablePosition}>C</div>
+          </div>
+          <nav className={styles.bilderNavTitles}>
+            <div className={styles.tableElement}>
+              <ScoreResumeCircle
+                  size={50}
+                  strokeWidth={5}
+                  progress={89}
+                  colorProgress='#958060'
+              >
+                  <div className={styles.sectResumeDescScore}>
+                      <p className={styles.sectResumeScorePercent}>89</p>
+                      <p className={styles.sectResumeScoreText}>100</p>
+                  </div>
+              </ScoreResumeCircle>
+            </div>
+            <div className={styles.tableElement}>
+              <div className="">28 Fabruary 2024</div>
+            </div>
+            <div className={styles.tableElement}>
+              <div className="">23 March 2024</div>
+            </div>
+          </nav>
+          <div className={styles.tableElement}>
+            <a href="#" className={styles.tableAction}><Remove/></a>
+            <a href="#" className={styles.tableAction}><Block/></a>
+            <a href="#" className={styles.tableAction}><Copy/></a>
+            <a href="#" className={styles.tableAction}><Edit/></a>
+          </div>
+        </div>
+        <div className={styles.bilderTableContainer}>
+          <div className={styles.tableElement}>
+            <input type="checkbox" name="" id="" className={styles.checkBox} />
+            <div className={styles.bilderTablePosition}>looooooooooooooooooooooooooooong names</div>
+          </div>
+          <nav className={styles.bilderNavTitles}>
+            <div className={styles.tableElement}>
+              <ScoreResumeCircle
+                  size={50}
+                  strokeWidth={5}
+                  progress={99}
+                  colorProgress='#958060'
+              >
+                  <div className={styles.sectResumeDescScore}>
+                      <p className={styles.sectResumeScorePercent}>99</p>
+                      <p className={styles.sectResumeScoreText}>100</p>
+                  </div>
+              </ScoreResumeCircle>
+            </div>
+            <div className={styles.tableElement}>
+              <div className="">28 Fabruary 2024</div>
+            </div>
+            <div className={styles.tableElement}>
+              <div className="">23 March 2024</div>
+            </div>
+          </nav>
+          <div className={styles.tableElement}>
+            <a href="#" className={styles.tableAction}><Remove/></a>
+            <a href="#" className={styles.tableAction}><Block/></a>
+            <a href="#" className={styles.tableAction}><Copy/></a>
+            <a href="#" className={styles.tableAction}><Edit/></a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default Builder;
+
+
+
