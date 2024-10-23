80f819d9a00c2f1848928d5f4b1dc41bb51a8443
Tiurne Viktoriya <tiurne@ukr.net>
2024-10-23 16:48:24 +0300

    merge commit

--- client/src/components/UI/CalendarUI/CalendarUI.jsx
+++ client/src/components/UI/CalendarUI/CalendarUI.jsx
@@ -1,38 +1,39 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Calendar from 'react-calendar'
-import './Calendar.css'
-import { ReactComponent as CalendarIcon } from '../../../assets/user_page/home/calendar.svg'
+import React from "react";
+import PropTypes from "prop-types";
+import Calendar from "react-calendar";
+import "./Calendar.css";
+import { ReactComponent as CalendarIcon } from "../../../assets/user_page/home/calendar.svg";
 
 const CalendarUI = (props) => {
-    const { date, ...restProps } = props
-    return (<>
-        <Calendar
-            next2Label={null}
-            prev2Label={null}
-            nextLabel={null}
-            prevLabel={null}
-            locale='en'
-            showNeighboringMonth={true}
-            value={date}
-            navigationLabel={() => {
-                return <>
-                    <CalendarIcon />
-                    <span>{date.toLocaleDateString('en-US', {
-                        month: 'long',
-                        day: 'numeric',
-                        year: 'numeric'
-                    })}   </span>
-
-                </>
-            }}
-            {...restProps}
-        />
-
+  const { date, ...restProps } = props;
+  return (
+    <>
+      <Calendar
+        next2Label={null}
+        prev2Label={null}
+        nextLabel={null}
+        prevLabel={null}
+        locale="en"
+        showNeighboringMonth={true}
+        value={date}
+        navigationLabel={() => {
+          return (
+            <>
+              <CalendarIcon />
+              <span>
+                {date.toLocaleDateString("en-US", {
+                  month: "long",
+                  day: "numeric",
+                  year: "numeric",
+                })}{" "}
+              </span>
+            </>
+          );
+        }}
+        {...restProps}
+      />
     </>
-
-
-    );
+  );
 };
 
 CalendarUI.propTypes = {};
