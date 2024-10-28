28ca5cedc182e804f33f1f86ac70fe8b512ef78b
Tiurne Viktoriya <tiurne@ukr.net>
2024-10-28 18:44:37 +0200

    change preview

--- client/src/pages/UserPage/Main/NewResume/NewResume.jsx
+++ client/src/pages/UserPage/Main/NewResume/NewResume.jsx
@@ -1,7 +1,6 @@
 import EnteringForm from "./EnteringForm/EnteringForm";
 import Preview from "./Preview/Preview";
-import Button from "../../../../components/UI/Button/Button";
-import exportImg from "../../../../assets/user_page/builder/createResume/export.svg";
+
 
 import styles from "./NewResume.module.css";
 
@@ -14,17 +13,7 @@ const NewResume = () => {
 
       <div className={styles.previewWrapper}>
         <Preview />
-        <div className={styles.exportBtns}>
-        <Button className={`${styles.export} ${styles.exportDoc}`}>
-         <img src={exportImg} alt="export" />
-         <p>Export in DOC</p> 
-        </Button>
-        <Button className={`${styles.export} ${styles.exportPdf}`}>
-        <img src={exportImg} alt="export" />
-         <p>Export in PDF</p> 
-        </Button>
-
-        </div>
+       
       </div>
     </div>
   );
