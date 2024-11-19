63949cd4059f3be4472281c784d6aa43470d74e6
Serhiy Shakhaiev <shakhaiev.s@gmail.com>
2024-11-19 23:46:13 +0200

    adaptive new desigh component search

--- client/src/components/UI/SearchBox/SearchBox.jsx
+++ client/src/components/UI/SearchBox/SearchBox.jsx
@@ -4,13 +4,12 @@ import { ReactComponent as Close } from "../../../assets/user_page/home/close.sv
 import styles from "./SearchBox.module.css";
 import cn from 'classnames'
 import { useMedia } from '../../../hoc/useMedia/useMedia.js'
-import Button from "../Button/Button";
+
 
 const SearchBox = () => {
   const [searchText, setSearchText] = useState("");
   const [isInputFocus, setIsInputFocus] = useState(true)
-  const [showInputForMobile, setShowInputForMobile] = useState(false)
-  const isMediaQuery = useMedia('(max-width:1150px)')
+
 
   const changeHandler = (e) => {
     const text = e.target.value;
@@ -22,52 +21,20 @@ const SearchBox = () => {
   }
 
   return (
-    <>
-      {isMediaQuery ? <>
-        <button
-          type="button"
-          className={styles.btnIconSearchMobile}
-          onClick={() => { setShowInputForMobile(!showInputForMobile) }}
-        >
-          <Search />
-        </button>
-        <div className={cn(styles.searchMobile,
-          { [styles.searchFocus]: !isInputFocus },
-          { [styles.searchMobileShow]: showInputForMobile && isMediaQuery }
-        )}>
-          <input
-            type="text"
-            placeholder="Resume search"
-            className={styles.searchInputMobile}
-            value={searchText}
-            onChange={changeHandler}
-          // onBlur={handleBlurInput}
-          // onFocus={() => { setIsInputFocus(false) }}
-          />
-          <button type="button"
-            className={styles.searchIconCloseMobile}
-            onClick={() => {
-              setSearchText('')
-              setShowInputForMobile(false)
-            }}
-          > <Close /></button>
-        </div>
-      </> : <div className={cn(styles.search,
-        { [styles.searchFocus]: !isInputFocus },
-      )}>
-        <input
-          type="text"
-          placeholder="Resume search"
-          className={styles.searchInput}
-          value={searchText}
-          onChange={changeHandler}
-          onBlur={handleBlurInput}
-          onFocus={() => { setIsInputFocus(false) }}
-        />
-        {isInputFocus && <Search />}
-      </div>
-      }
-    </>
+    <div className={cn(styles.search,
+      { [styles.searchFocus]: !isInputFocus },
+    )}>
+      <input
+        type="text"
+        placeholder="Resume search"
+        className={styles.searchInput}
+        value={searchText}
+        onChange={changeHandler}
+        onBlur={handleBlurInput}
+        onFocus={() => { setIsInputFocus(false) }}
+      />
+      <Search />
+    </div>
   );
 };
 
