9f663e87397714375186a34bbb1711141718f86c
Tiurne Viktoriya <tiurne@ukr.net>
2024-11-20 15:44:21 +0200

    change logo, fix adaptiv

--- landing/src/http/index.js
+++ landing/src/http/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const $host = axios.create({
-    baseURL: "http://localhost:5000", // Нужно загнать в конфиг фаил это римерно process.env.REACT_APP_API_URL
+    baseURL: process.env.REACT_APP_API_URL
 })
 
 export {
