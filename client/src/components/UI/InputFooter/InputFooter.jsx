df3a11c402d3a3dc4ae965d1412220ae94a4166b
Tiurne Viktoriya <tiurne@ukr.net>
2024-11-22 16:37:58 +0200

    validation footerform, set up links

---
+++ client/src/components/UI/InputFooter/InputFooter.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+import { Field } from "formik";
+
+import crossIcon from "../../../assets/crossIcon.svg";
+import checkIcon from "../../../assets/checkIcon.svg";
+
+import styles from "./InputFooter.module.css";
+
+const InputFooter = ({ id, name, placeholder, img, error, touched, values }) => {
+    const [isHideErrMsg, setIsHideErrMsg] = useState(false);
+
+  useEffect(() => {
+    error[name] ? setIsHideErrMsg(false) : setIsHideErrMsg(true);
+  }, [error, name]);
+
+  return (
+    <div className={styles.inputWrapper}>
+          <div className={img?styles.inputContainer:styles.inputText}>
+              
+        {img && (!touched[name] ? (
+          <img src={img} alt={img} className={styles.icon} />
+        ) : error[name] ? (
+          <img src={crossIcon} alt="cross" />
+        ) : (
+          <img src={checkIcon} alt="check" />
+        ))}
+
+        <Field
+          autoComplete="off"
+          name={name}
+          id={id}
+          placeholder={placeholder}
+          className={
+            !touched[name] || !error[name] || isHideErrMsg
+            ? values[name] !== ""
+              ? error[name]
+                ? styles.inputFieldErr
+                : styles.inputFieldCorrect
+              : styles.inputField
+            : `${styles.inputField} ${styles.inputNotvisible}`
+          }
+        />
+      </div>
+      {error[name] && touched[name] ? (
+        <div
+          onClick={() => setIsHideErrMsg(true)}
+          className={isHideErrMsg ? styles.hideErrField : styles.errField}
+        >
+          <p>{error[name]}</p>
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default InputFooter;
