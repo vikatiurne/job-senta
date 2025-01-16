ab0fc7c80ec1206a56ef1fa4e126b4d2ebccfb6c
Tiurne Viktoriya <tiurne@ukr.net>
2025-01-16 15:58:24 +0200

    cookie

--- server/index.js
+++ server/index.js
@@ -28,7 +28,8 @@ app.use(
     secret: process.env.AUTH0_CLIENT_SECRET,
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: false, httpOnly: true },
+    cookie: { secure: true, httpOnly: true },
+    // cookie: { secure: false, httpOnly: true },
   })
 );
 app.use(passport.initialize());
