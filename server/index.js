6cda517a9908525e03237ce925ccd991b93b1989
Tiurne Viktoriya <tiurne@ukr.net>
2025-01-16 16:05:08 +0200

    method cors

--- server/index.js
+++ server/index.js
@@ -17,6 +17,7 @@ const app = express();
 app.use(
   cors({
     origin: [process.env.CLIENT_URL, process.env.API_URL],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
     optionSuccessStatus: 200,
   })
